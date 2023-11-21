83b50d17f4a7e638e7e36716e2fa697cc0743baa
eoeo777755@naver.com <eoeo777755@naver.com>
2023-11-21 22:44:10 +0900

    추가

--- src/main/java/velog/sideProject/repository/member/MemberRepository.java
+++ src/main/java/velog/sideProject/repository/member/MemberRepository.java
@@ -1,7 +1,16 @@
 package velog.sideProject.repository.member;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import velog.sideProject.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findByMemberEmail(String email);
+
+    boolean existsByMemberEmail(String email);
+    @Query("select m.memberId from Member m where m.memberEmail = :email")
+    Long findIdByMemberEmail(String email);
 }
